129ec2aa67415d03dd258e19815ac230e9f2c64d
ZLUP <i.am.sachkov@gmail.com>
2026-03-28 17:37:43 +0500

    lab8

---
+++ lab8/shop-back/api/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .views import Product, Category
+
+# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "name", "price", "count", "is_active", "category")
+    list_filter = ("is_active", "category")
+    search_fields = ("name",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
